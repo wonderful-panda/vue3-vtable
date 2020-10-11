4eae7e4e9cde153fcd3c9c6578df86aa64a9273c
iwata hidetaka <iwata0303@gmail.com>
2020-10-11 16:26:11 +0900

    rename example

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from "vue";
-import App from "./App";
+import VtreeTableExample from "./VtreeTableExample";
 
-createApp(App).mount("#app");
+createApp(VtreeTableExample).mount("#app");
